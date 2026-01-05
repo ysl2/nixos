f6861b6d9d3f306bba82e84b9978d592251598f8
ysl2 <songliyu97@gmail.com>
2026-01-06 00:01:10 +0800

    Set no need sudo password.

--- configuration.nix
+++ configuration.nix
@@ -80,6 +80,8 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
+  security.sudo.wheelNeedsPassword = false;
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.songliyu = {
     isNormalUser = true;
