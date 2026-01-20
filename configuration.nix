ee1d1932844c24206cc1e32c2b7a59b8c38845e0
ysl2 <songliyu97@gmail.com>
2026-01-20 23:07:14 +0800

    Enable nix-ld for installing miniforge3.

--- configuration.nix
+++ configuration.nix
@@ -133,6 +133,8 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  programs.nix-ld.enable = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
